63700b4cd3083d1c492a00a26f53a48293fdf1f3
GeronXXX <lityagin.ru@gmail.com>
2023-03-10 22:28:21 +0300

    v3

--- src/test/java/pages/PageObgects.java
+++ src/test/java/pages/PageObgects.java
@@ -8,7 +8,6 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-
 public class PageObgects {
     CalendarComponent calendarComponent = new CalendarComponent();
     RegistrationResultsModal registrationResultsModal = new RegistrationResultsModal();
